62bb9a0a541ad09a11a79721c497fef6f330961d
chunc <fsprogamer@gmail.com>
2020-01-19 21:44:56 +0300

    First

---
+++ RedisWebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisWebApplication.Services;
+using Serilog;
+
+namespace RedisWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly RedisService _redisService;
+        public ValuesController(RedisService redisService)
+        {
+            // Set RedisService property
+            _redisService = redisService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<int>> GetAsync(int id)
+        {
+            await _redisService.Set("How many claps per person should this article get?", $"{id}");
+            var definitely = await _redisService.Get("Follow me for more programming made simple articles");
+            Log.Information(definitely);
+            return id;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public ActionResult<string> Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ RedisWebApplication/Services/RedisService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+namespace RedisWebApplication.Services
+{
+    public class RedisService
+    {
+        private readonly string _redisHost;
+        private readonly int _redisPort;
+
+        private ConnectionMultiplexer _redis;
+
+        public RedisService(IConfiguration config)
+        {
+            _redisHost = config["Redis:Host"];
+            _redisPort = Convert.ToInt32(config["Redis:Port"]);
+        }
+        public void Connect()
+        {
+            try
+            {
+                var configString = $"{_redisHost}:{_redisPort},connectRetry=3";
+                _redis = ConnectionMultiplexer.Connect(configString);
+            }
+            catch (RedisConnectionException err)
+            {
+                Log.Error(err.ToString());
+                throw err;
+            }
+            Log.Debug("Connected to Redis");
+        }
+        public async Task<bool> Set(string key, string value)
+        {
+            var db = _redis.GetDatabase();
+            return await db.StringSetAsync(key, value);
+        }
+
+        public async Task<string> Get(string key)
+        {
+            var db = _redis.GetDatabase();
+            return await db.StringGetAsync(key);
+        }
+    }
+}
